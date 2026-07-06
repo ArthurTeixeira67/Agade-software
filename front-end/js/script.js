7d4c3cacd3a1420296229bffd63ff514f149b838
arthurteixeiragit11-a11y <arthur.teixeira.git11@gmail.com>
2026-07-06 17:06:06 -0300

    feat: adição da chamada do ckan no arquivo js, ao colocar o link de teste ele retorna um array com os dados

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,1 +1,33 @@
 console.log("Olá Agadê!")
+
+const bt_listar = document.getElementById('bt-listar');
+const input_package = document.getElementById('package');
+
+bt_listar.addEventListener('click', async function buscar_package() {
+    
+    const input_url = input_package.value.trim();
+    
+    if(!input_url){
+
+        alert("Por favor insira uma url valida!");
+        return;
+    }
+
+    const url_obj = new URL(input_url);
+    const api_base = url_obj.origin + '/api/3/action';
+    
+    try {
+        
+        const busca_lista = await fetch(`${api_base}/package_list`);
+        const json_lista = await busca_lista.json()
+        const package_id = json_lista.result;
+
+        console.log(package_id)
+        console.log("deu certo :)")
+
+    } catch (error) {
+        console.log("Deu ruim :(")
+    }
+});
+
+// p/ testar a chamada do CKAN no input 'Url': https://demo.ckan.org
