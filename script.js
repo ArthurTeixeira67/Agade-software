72446f7c4fe3943f277fc590a5c97dd86d3a486a
arthurteixeiragit11-a11y <arthur.teixeira.git11@gmail.com>
2026-08-31 12:17:39 -0300

    reiniciando o js, já esta colocado a verificação de campo não preenchido

---
+++ script.js
@@ -0,0 +1,35 @@
+console.log(`Olá Agadê!`);
+
+// guardando elementos de botões e input's em variaveis e objetos.
+const btn_package = $("#btn_package");
+const url_package = $("#url_package");
+const campos = [
+    $("#id_base"),
+    $("#nome"),
+    $("#descricao"),
+    $("#tabela_destino"),
+    $("#fonte"),
+    $("#fonte_link"),
+    $("#fonte_api")
+];
+
+// caso o botão 'Criar bases e fontes' seja clicado.
+$("form").on("submit", function(event) {
+    // flag para validar campos.
+    let valido = true;
+
+    // percorre cada campo e verifica se está preenchido (caso não, marca o campo em vermelho e aciona um alerta).
+    campos.forEach(function(campo){
+        if (!campo.val()) {
+            campo.addClass("campo-invalido");
+            valido = false;
+        }
+    });
+
+    if(!valido) {
+        event.preventDefault();
+        alert("Preencha todos os campos obrigatórios!")
+    }
+
+});
+
